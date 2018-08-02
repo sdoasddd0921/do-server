211e5684bff5b1ad1e0eb1ba4c93991dd848894a
ZQ-MBP <850307035@qq.com>
2018-08-02 23:01:51 +0800

    change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const os = require('os')
 const app = express()
 const hostServer = ['nk']
 
-const isServer = hostServer.indexOf(oa.hostname()) !== -1
+const isServer = hostServer.indexOf(os.hostname()) !== -1
 const port = isServer ? 80 : 3032
 
 app.get('/', (req, res) => {
