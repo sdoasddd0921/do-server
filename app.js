67d12b68072ab30aa73783a64ef73bf8fc4c1a10
ZQ-MBP <850307035@qq.com>
2018-08-02 22:43:13 +0800

    start test

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const oa = require('oa')
+
+const app = express()
+const hostServer = ['nk']
+
+const isServer = hostServer.indexOf(oa.hostname()) !== -1
+const port = isServer ? 80 : 3032
+
+app.get('/', (req, res) => {
+  res.send('node server opened.')
+})
+
+app.listen(port, () => {
+  console.log('servre started at port: ' + port)
+})
